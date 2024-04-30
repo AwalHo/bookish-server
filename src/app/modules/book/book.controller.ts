725b21d8ede97b9307e511db389bfbb36f74da4a
awalhossain <waqin21@gmail.com>
2024-04-30 21:48:28 +0600

    Refactor book.controller.ts for improved readability and removed unnecessary console.log statements

--- src/app/modules/book/book.controller.ts
+++ src/app/modules/book/book.controller.ts
@@ -9,7 +9,7 @@ import { bookService } from './book.service';
 
 const addBook = catchAsync(async (req: Request, res: Response) => {
   const book = req.body;
-  
+
   const result = await bookService.addBook(book);
 
   sendResponse(res, {
@@ -21,13 +21,11 @@ const addBook = catchAsync(async (req: Request, res: Response) => {
 });
 
 const getAllBooks = catchAsync(async (req: Request, res: Response) => {
-  
   const filters = pick(req.query, bookFilterableFields);
   const paginationOptions = pick(req.query, paginationFields);
-  console.log(filters,'che', paginationOptions);
-  
-  const result = await bookService.getAllBooks(filters, paginationOptions);
+  console.log(filters, 'che', paginationOptions);
 
+  const result = await bookService.getAllBooks(filters, paginationOptions);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
@@ -35,28 +33,22 @@ const getAllBooks = catchAsync(async (req: Request, res: Response) => {
     message: 'All books',
     data: result,
   });
-}
-);
+});
 
 const getSingleBook = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await bookService.getSingleBook(id);
 
-    const { id } = req.params;
-    const result = await bookService.getSingleBook(id);
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Retrieved single book',
-      data: result,
-    });
-
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Retrieved single book',
+    data: result,
+  });
 });
 
 const addReview = catchAsync(async (req: Request, res: Response) => {
-  const {bookId} = req.params;
-  const user = req.user;
-  const {review} = req.body;
-  const result = await bookService.addReview(review, user, bookId);
+  const result = await bookService.addReview(req);
 
   sendResponse(res, {
     statusCode: httpStatus.CREATED,
@@ -64,15 +56,13 @@ const addReview = catchAsync(async (req: Request, res: Response) => {
     message: 'Review added successfully',
     data: result,
   });
-}
-);
-
+});
 
 const editBook = catchAsync(async (req: Request, res: Response) => {
   const { bookId } = req.params;
   const data = req.body;
-  console.log(data,'cheki', bookId);
-  
+  console.log(data, 'cheki', bookId);
+
   const result = await bookService.editBook(bookId, data);
 
   sendResponse(res, {
@@ -81,8 +71,7 @@ const editBook = catchAsync(async (req: Request, res: Response) => {
     message: 'Book updated successfully',
     data: result,
   });
-})
-
+});
 
 const deleteBook = catchAsync(async (req: Request, res: Response) => {
   const { bookId } = req.params;
@@ -94,7 +83,31 @@ const deleteBook = catchAsync(async (req: Request, res: Response) => {
     message: 'Book deleted successfully',
     data: result,
   });
-})
+});
+
+const addUserPreference = catchAsync(async (req: Request, res: Response) => {
+  const result = await bookService.addUserPreference(req);
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'User preference added successfully',
+    data: result,
+  });
+});
+
+// const updateUserPreference = catchAsync(async (req: Request, res: Response) => {
+//   const { bookId } = req.params;
+//   const { status } = req.body;
+//   const userId = req.user._id; // Assuming you have middleware to get the authenticated user
+
+//   const result = await bookService.updateUserPreference(bookId, userId, status);
+//   sendResponse(res, {
+//     statusCode: httpStatus.OK,
+//     success: true,
+//     message: 'User preference updated successfully',
+//     data: result,
+//   });
+// });
 
 export const BookController = {
   addBook,
@@ -103,4 +116,5 @@ export const BookController = {
   addReview,
   editBook,
   deleteBook,
+  addUserPreference,
 };
