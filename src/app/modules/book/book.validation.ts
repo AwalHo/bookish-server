2429898b694d8f786ef96dfd9731a96232c1ccb0
awalhossain <waqin21@gmail.com>
2024-05-05 10:41:30 +0600

    Refactor book.validation.ts to use single quotes instead of double quotes

--- src/app/modules/book/book.validation.ts
+++ src/app/modules/book/book.validation.ts
@@ -1,19 +1,22 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 const bookZodSchema = z.object({
-    body: z.object({
-        title: z.string(),
-        author: z.string(),
-        thumbnail: z.string(),
-        genre: z.string(),
-        publicationYear: z.string(),
-        reviews: z.array(z.object({
+  body: z.object({
+    title: z.string(),
+    author: z.string(),
+    thumbnail: z.string(),
+    genre: z.array(z.string()),
+    publicationYear: z.string().transform(value => new Date(value)),
+    reviews: z
+      .array(
+        z.object({
           // Define the properties of the reviews here, if needed
-        })).optional(),
-    })
+        })
+      )
+      .optional(),
+  }),
 });
 
-
 export const bookValidation = {
-    bookZodSchema,
+  bookZodSchema,
 };
